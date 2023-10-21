60a5b13255c9eb3fa614881e0fb0dc911030dff5
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-10-21 12:43:39 +0600

    Brands section has been added to enhance better user experience

--- src/Pages/Home/Brands/BrandCard.jsx
+++ src/Pages/Home/Brands/BrandCard.jsx
@@ -6,7 +6,15 @@ const BrandCard = ({brand}) => {
     const {id, name, image} = brand;
     console.log(brand)
     return (
-
+        <div className="card bg-base-100 border rounded-xl transition-tranform duration-500 hover:shadow-2xl transform hover:-translate-y-1">
+            <figure data-aos="zoom-in"><img src={image} alt="image" className="w-full h-56"/></figure>
+            <div data-aos="zoom-in" className="card-body p-5">
+                <h2 className="text-xl md:text-2xl font-semibold text-center">{name}</h2>
+                <div className="card-actions justify-center mt-2">
+                    <NavLink to={`/event/${id}`} className="btn btn-neutral border-0 bg-primary text-white transition duration-300 w-full">Learn More</NavLink>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/Pages/Home/Brands/Brands.jsx
+++ src/Pages/Home/Brands/Brands.jsx
@@ -4,7 +4,14 @@ import BrandCard from './BrandCard';
 const Brands = ({brands}) => {
 
     return (
-
+        <div className='max-w-6xl mx-auto py-16 px-4 md:px-6 lg:px-8'>
+            <h1 className="text-3xl md:text-4xl font-bold text-center py-2 pb-16"><span className="text-primary">Brands</span> We Provide</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {
+                    brands.map(brand => <BrandCard key={brand.id} brand={brand}></BrandCard>)
+                }
+            </div>
+        </div>
     );
 };
 
